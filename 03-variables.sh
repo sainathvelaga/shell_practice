ca8a67709508542def651c42454846257923ae03
sainath <sainath.velaga@gmail.com>
2024-04-21 14:33:43 +0530

    hello world

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=ramesh
+person2=suresh
+
+echo "$person1:: Hello $person2, How are you?"
+echo "$person2:: Hi $person1, I am fine. How are you?"
+echo "$person1: I am fine too. how is your work?"
+echo "$person2:: not bad. I am thinking to upgrade to DevOps"
